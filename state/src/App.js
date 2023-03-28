3b4bb04c4cd7f34977967f0fc6031210b3892921
alperengonull <81569125+alperengonull@users.noreply.github.com>
2023-03-28 20:26:09 +0300

    Update App.js

--- state/src/App.js
+++ state/src/App.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState,useEffect} from 'react';
+import {View, Text,Button} from 'react-native';
+
 
 const App = () => {
+  
+  const [helloFlag,setHelloFlag] = useState(true)
+
+  function updateFlag(){
+    setHelloFlag(!helloFlag)
+  }
+
   return (
     <View>
-      <Text>App</Text>
+     <Text>Hello life cycle</Text>
+      <Button title='Click' onPress={updateFlag} />
+     {helloFlag && <Hello /> }   
+     {/* conditional views (kondisyonel görüntüleme) */}
+    </View>
+  );
+};
+
+export default App;
+
+
+function Hello(){
+
+  // Componentin kaldırılma yeri
+  useEffect(()=>{
+    console.log("Merhaba Dünya");
+
+ 
+    return () => {
+      console.log("Gidiyorum bütün aşklar yüreğimde");
+    }
+
+  }, []);
+
+  return (
+    <View style={{backgroundColor:'aqua',padding:20}}>
+      <Text >Hello Componenti</Text>
     </View>
   )
 }
-
-export default App;
